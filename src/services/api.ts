2a21271421c8c30498a4442e31bfa3a1b56c6aee
mrullldhm <mrullldhm94@gmail.com>
2025-08-07 00:34:09 +0800

    Initialize api.ts

---
+++ src/services/api.ts
@@ -0,0 +1,18 @@
+const API_KEY = "727e4337d902722c7d0a021f7e61e746";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export async function getPopularMovies() {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+}
+
+export async function searchMovies(query: string) {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+}
